df8834a547252ca2d924c50b60ea501ec4ff2aa3
Andrew Dominguez <adominguez24@usf.edu>
2026-06-14 15:21:25 -0400

    Update cc_7.js

--- cc_7.js
+++ cc_7.js
@@ -1,1 +1,7 @@
+const input = document.getElementById("ctaInput");
+const button = document.getElementById("updateBtn");
+const ctaText = document.getElementById("ctaText");
 
+button.addEventListener("click", function () {
+    ctaText.textContent = input.value;
+});
